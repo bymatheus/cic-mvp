2c30453925a742604f23eb63cbc44191cc7eb8ed
Matheus Xavier <matheuscx@outlook.com>
2022-09-20 18:59:04 -0300

    chore(front): tagueando homepage

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,3 +1,150 @@
 <x-layout title="Bem vindo(a) ao CIC">
 
+  <header class="container-fluid">
+    <div class="row">
+      <section class="col-6">
+        <h6></h6>
+        <h1></h1>
+        <p></p>
+        <a href="" title=""></a>
+      </section>
+    </div>
+  </header>
+
+  <aside class="container-fluid">
+    <div class="row">
+      <section class="col-6">
+        <small></small>
+        <h5></h5>
+        <p></p>
+        <p></p>
+      </section>
+
+      <section class="col-6">
+        <div class="ratio ratio-16x9">
+          <iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="YouTube video" allowfullscreen></iframe>
+        </div>
+      </section>
+    </div>
+  </aside>
+
+  <article class="container-fluid">
+    <div class="row">
+      <div class="col-7">
+        <div class="row">
+          <section class="col-12">
+            <small></small>
+            <h2></h2>
+          </section>
+
+          <section class="col-6">
+            <div class="d-flex">
+              <img src="" alt="">
+              <h6></h6>
+            </div>
+
+            <p>
+
+            </p>
+          </section>
+
+          <section class="col-6">
+            <div class="d-flex">
+              <img src="" alt="">
+              <h6></h6>
+            </div>
+
+            <p>
+
+            </p>
+          </section>
+
+          <section class="col-6">
+            <div class="d-flex">
+              <img src="" alt="">
+              <h6></h6>
+            </div>
+
+            <p>
+
+            </p>
+          </section>
+
+          <section class="col-6">
+            <div class="d-flex">
+              <img src="" alt="">
+              <h6></h6>
+            </div>
+
+            <p>
+
+            </p>
+          </section>
+        </div>
+      </div>
+
+      <div class="col-5">
+        <img src="" alt="">
+      </div>
+    </div>
+  </article>
+
+  <aside class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <small></small>
+        <h5></h5>
+      </div>
+
+      <div class="col-12 d-flex">
+        <img src="" alt="">
+        <img src="" alt="">
+        <img src="" alt="">
+        <img src="" alt="">
+      </div>
+    </div>
+  </aside>
+
+  <footer class="container-fluid">
+    <div class="row">
+      <div class="col-5">
+        <div class="row">
+          <div class="col-4">
+            <h6></h6>
+            <p></p>
+
+            <h6></h6>
+            <p></p>
+
+            <h6></h6>
+            <p></p>
+          </div>
+
+          <div class="col-8">
+            <img src="" alt="">
+          </div>
+        </div>
+      </div>
+
+      <div class="col-7">
+        <form>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+          </div>
+          <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    </div>
+  </footer>
+
 </x-layout>
