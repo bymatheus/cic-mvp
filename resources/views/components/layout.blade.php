ddb2daaca2807154063c17906a3dbdb6ca4338c1
Matheus Xavier <matheuscx@outlook.com>
2022-09-20 18:33:17 -0300

    chore(front): adicionando metas de share nas redes sociais

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,8 +6,23 @@
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>{{ $title }}</title>
+        <!-- Facebook -->
+        <meta property="og:title"          content="">
+        <meta property="og:description"    content="">
+        <meta property="og:site_name"      content="">
+        <meta property="og:url"            content="">
+        <meta property="og:image"          content="">
+        <meta property="og:image:type"     content="image/png">
+        <meta property="og:image:width"    content="800">
+        <meta property="og:image:height"   content="600">
+        <meta property="og:type"           content="website">
+        <!-- Twitter -->
+        <meta name="twitter:card"          content="summary">
+        <meta name="twitter:title"         content="">
+        <meta name="twitter:description"   content="">
+        <meta name="twitter:image"         content="">
     </head>
-    <body>
+    <body id="top">
         {{ $slot }}
     </body>
 </html>
