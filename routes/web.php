f5beabed9cac85174d8ea6c2f67167ccf9c80bcb
Matheus Xavier <matheuscx@outlook.com>
2022-09-20 18:20:37 -0300

    chore(front): criando rota da homepage

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LandingPage\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,3 +13,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::controller(HomeController::class)->group(function () {
+    Route::get('', 'index')->name('home.index');
+});
