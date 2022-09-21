301363c403c8f3ca00cb15c5f16b584e708c870b
Matheus Xavier <matheuscx@outlook.com>
2022-09-21 20:14:41 -0300

    chore(env): linkando logo e alterando para sua versão branca

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -25,8 +25,8 @@
   </head>
   <body id="top">
     <nav class="navbar navbar-expand-lg fixed-top">
-      <a class="navbar-brand" href="#">
-        <img src="{{ asset('dist/images/cic.svg') }}" alt="CIC">
+      <a class="navbar-brand" href="{{ route('home.index') }}">
+        <img src="{{ asset('dist/images/cic-branco.svg') }}" alt="CIC">
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
