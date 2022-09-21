7e60dce3209d49fef973c2034d992cd7d78ae2b9
Matheus Xavier <matheuscx@outlook.com>
2022-09-20 22:01:42 -0300

    chore(front): criando estilo do menu

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -22,7 +22,7 @@
 
       <section class="col-6">
         <div class="ratio ratio-16x9">
-          <iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="YouTube video" allowfullscreen></iframe>
+
         </div>
       </section>
     </div>
