5700e110da107a84d5e192b571a25a3c2ee5ce5e
Matheus Xavier <matheuscx@outlook.com>
2022-09-27 22:41:16 -0300

    chore(front): criando rota de contato

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\LandingPage\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 Route::controller(HomeController::class)->group(function () {
     Route::get('', 'index')->name('home.index');
+});
+
+Route::controller(ContactController::class)->group(function () {
+    Route::post('', 'store')->name('contact.store');
 });
