5069dfaabb14df6d9c0a12f45c99118695cbdfb8
Matheus Xavier <matheuscx@outlook.com>
2022-09-28 19:55:40 -0300

    chore(front): criando rotas de dashboard, login e logout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Dashboard\LogoutController;
 use App\Http\Controllers\LandingPage\ContactController;
 use App\Http\Controllers\LandingPage\HomeController;
+use App\Http\Controllers\Dashboard\HomeController as HomeDashboardController;
+use App\Http\Controllers\LandingPage\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,4 +23,22 @@ Route::controller(HomeController::class)->group(function () {
 
 Route::controller(ContactController::class)->group(function () {
     Route::post('/contato', 'store')->name('contact.store');
+});
+
+Route::controller(LoginController::class)->group(function () {
+    Route::get('/login', 'index')->name('login.index');
+    Route::post('/login', 'store')->name('login.store');
+});
+
+/*
+|--------------------------------------------------------------------------
+| Dashboard
+|--------------------------------------------------------------------------
+*/
+Route::controller(HomeDashboardController::class)->group(function () {
+    Route::get('/dashboard', 'index')->name('dashboard.home.index');
+});
+
+Route::controller(LogoutController::class)->group(function () {
+    Route::delete('logout/', 'index')->name('dashboard.logout.index');
 });
