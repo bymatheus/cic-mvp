bbd907679f30a603cf0a0c271eaefd113343966e
Matheus Xavier <matheuscx@outlook.com>
2022-09-29 20:54:16 -0300

    chore(front): criando layout padrão da dashboard

--- app/Http/Controllers/Dashboard/HomeController.php
+++ app/Http/Controllers/Dashboard/HomeController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index(): Factory | View | Application
+    {
+        return view('dashboard.home.index');
+    }
 }
---
+++ resources/views/dashboard/home/index.blade.php
@@ -0,0 +1,5 @@
+<x-layout title="Bem vindo(a) ao CIC" key="0" dashboard="1">
+  <main class="dashboard container-fluid">
+
+  </main>
+</x-layout>
