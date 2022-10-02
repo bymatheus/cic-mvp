59e2a03aaeb7db916bc820712f9e3d024736dce0
Matheus Xavier <matheuscx@outlook.com>
2022-10-02 19:08:18 -0300

    chore(front): criando cards da homepage

--- resources/views/dashboard/home/index.blade.php
+++ resources/views/dashboard/home/index.blade.php
@@ -1,3 +1,217 @@
 <x-layout title="Bem vindo(a) ao CIC" key="0" dashboard="1">
+  <div class="row home">
+    <div class="col-12 mb-2 d-flex">
+      <button type="button" class="ms-auto btn btn--mid-blue btn--small">
+        + filtros
+      </button>
+    </div>
 
+    <div class="col-6 home__cards">
+
+      <section class="dash-card">
+        <aside class="dash-card__color dash-card__color--green"></aside>
+
+        <section class="dash-card__content">
+          <div class="dash-card__content__title align-self-start">
+            <h6 class="m-0">
+              Pedidos de crédito
+            </h6>
+
+            <button type="button" class="ms-auto btn btn__text" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Help">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+              </svg>
+            </button>
+          </div>
+
+          <div class="dash-card__content__value align-self-end">
+            <p class="m-0">
+              3
+            </p>
+          </div>
+        </section>
+      </section>
+
+      <section class="dash-card">
+        <aside class="dash-card__color dash-card__color--green"></aside>
+
+        <section class="dash-card__content">
+          <div class="dash-card__content__title align-self-start">
+            <h6 class="m-0">
+              Processo administrativo
+            </h6>
+
+            <button type="button" class="ms-auto btn btn__text" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Help">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+              </svg>
+            </button>
+          </div>
+
+          <div class="dash-card__content__value align-self-end">
+            <p class="m-0">
+              0
+            </p>
+          </div>
+        </section>
+      </section>
+
+      <section class="dash-card">
+        <aside class="dash-card__color dash-card__color--blue"></aside>
+
+        <section class="dash-card__content">
+          <div class="dash-card__content__title align-self-start">
+            <h6 class="m-0">
+              Declarações de compesação
+            </h6>
+
+            <button type="button" class="ms-auto btn btn__text" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Help">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+              </svg>
+            </button>
+          </div>
+
+          <div class="dash-card__content__value align-self-end">
+            <p class="m-0">
+              44
+            </p>
+          </div>
+        </section>
+      </section>
+
+      <section class="dash-card">
+        <aside class="dash-card__color dash-card__color--blue"></aside>
+
+        <section class="dash-card__content">
+          <div class="dash-card__content__title align-self-start">
+            <h6 class="m-0">
+              Valor da reserva
+            </h6>
+
+            <button type="button" class="ms-auto btn btn__text" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Help">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+              </svg>
+            </button>
+          </div>
+
+          <div class="dash-card__content__value align-self-end">
+            <p class="m-0">
+              <small>R$ </small> 0,00
+            </p>
+          </div>
+        </section>
+      </section>
+
+      <section class="dash-card">
+        <aside class="dash-card__color dash-card__color--yellow"></aside>
+
+        <section class="dash-card__content">
+          <div class="dash-card__content__title align-self-start">
+            <h6 class="m-0">
+              Indeferimento em recursos
+            </h6>
+
+            <button type="button" class="ms-auto btn btn__text" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Help">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+              </svg>
+            </button>
+          </div>
+
+          <div class="dash-card__content__value align-self-end">
+            <p class="m-0">
+              <small>R$ </small> 0,00
+            </p>
+          </div>
+        </section>
+      </section>
+
+      <section class="dash-card">
+        <aside class="dash-card__color dash-card__color--red"></aside>
+
+        <section class="dash-card__content">
+          <div class="dash-card__content__title align-self-start">
+            <h6 class="m-0">
+              Multas
+            </h6>
+
+            <button type="button" class="ms-auto btn btn__text" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Help">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+              </svg>
+            </button>
+          </div>
+
+          <div class="dash-card__content__value align-self-end">
+            <p class="m-0">
+              <small>R$ </small> 0,00
+            </p>
+          </div>
+        </section>
+      </section>
+
+      <section class="dash-card">
+        <aside class="dash-card__color dash-card__color--red"></aside>
+
+        <section class="dash-card__content">
+          <div class="dash-card__content__title align-self-start">
+            <h6 class="m-0">
+              Multa isolada e juros
+            </h6>
+
+            <button type="button" class="ms-auto btn btn__text" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Help">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+              </svg>
+            </button>
+          </div>
+
+          <div class="dash-card__content__value align-self-end">
+            <p class="m-0">
+              <small>R$ </small> 0,00
+            </p>
+          </div>
+        </section>
+      </section>
+
+      <section class="dash-card">
+        <aside class="dash-card__color dash-card__color--red"></aside>
+
+        <section class="dash-card__content">
+          <div class="dash-card__content__title align-self-start">
+            <h6 class="m-0">
+              Contingencia atualizada
+            </h6>
+
+            <button type="button" class="ms-auto btn btn__text" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Help">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+              </svg>
+            </button>
+          </div>
+
+          <div class="dash-card__content__value align-self-end">
+            <p class="m-0">
+              <small>R$ </small> 0,00
+            </p>
+          </div>
+        </section>
+      </section>
+
+    </div>
+
+    <div class="col-6 home__info">
+
+    </div>
+  </div>
 </x-layout>
