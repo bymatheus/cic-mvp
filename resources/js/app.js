f204e9162b4b20b00e768ea521083525babc2d77
Matheus Xavier <matheuscx@outlook.com>
2022-10-05 00:22:32 -0300

    chore(front): criando collapse no menu

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,10 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+document.querySelector("#collapseNavbar").addEventListener("click", function () {
+  let dashboard = document.querySelector(".dashboard");
+  let nav = document.querySelector(".nav");
+
+  dashboard.classList.toggle('nav-collapsed');
+  nav.classList.toggle('nav-collapsed');
+});
